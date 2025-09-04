e2d5b7795eeed7b77d062c84fe70b398ccae0880
23335a0504raju <smilyraju8464@gmail.com>
2025-09-04 15:18:21 +0530

    changed requirements

--- object_detection/start.sh
+++ object_detection/start.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+
+# Exit on error
+set -e
+
+# Install dependencies (if Render doesn't automatically do it)
+pip install -r requirements.txt
+
+# Run migrations
+python manage.py migrate
+
+# Collect static files (optional)
 python manage.py collectstatic --noinput
+
+# Start server using Gunicorn on the assigned PORT
 gunicorn backend.wsgi:application --bind 0.0.0.0:$PORT
